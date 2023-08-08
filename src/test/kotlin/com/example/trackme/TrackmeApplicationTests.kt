63b37edf26a761167ea125c06169e34ddaf84050
Maciej Pawlik <maciejmpawlik@gmail.com>
2023-08-08 11:32:49 +0200

    initial

---
+++ src/test/kotlin/com/example/trackme/TrackmeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.trackme
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TrackmeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
